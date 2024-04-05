78a8f425c374a951304ce17bdc63729df0744148
Malicia Torrent <malicia.torrent@gmail.com>
2024-04-05 08:54:54 +0200

    send content

--- src/components/Mailbox/Message/Chat.tsx
+++ src/components/Mailbox/Message/Chat.tsx
@@ -1,30 +1,51 @@
-// import useStore from "@/hooks/useStore";
+import useStore from "@/hooks/useStore";
 import Image from "next/image";
 import ContentMessage from "./ContentMessage";
 import Send from "./Send";
+import { useEffect, useState } from "react";
+import { PrismaClient } from "@prisma/client";
 
-const Chat = () => {
-  // const { user } = useStore();
+const prisma = new PrismaClient();
+
+async function getData() {
+  const data = await prisma.messages.findMany();
+  return data;
+}
+
+export default function Chat() {
+  const { user } = useStore();
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getData();
+        setMessages(data);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des messages:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
-    <div className=" flex flex-1 flex-col overflow-y-scroll">
+    <div className="flex flex-1 flex-col overflow-y-scroll">
       <div className="flex flex-row items-center border-b border-solid border-black/20">
         <Image
-          src={"avatar.jpg"}
+          src={user?.profilePicture as string || "avatar.jpg"}
           alt="Profile picture"
           width={50}
           height={50}
-          className=" m-3 rounded-full"
+          className="m-3 rounded-full"
         />
-        <span className=" right-0 top-0 block size-2 rounded-full bg-green-500 ring-2 ring-white md:size-3 " />
+        <span className="right-0 top-0 block size-2 rounded-full bg-green-500 ring-2 ring-white md:size-3" />
         <p className="ml-3 border-l border-solid border-black/20 p-4 text-black">
           Pseudo
         </p>
       </div>
-      <ContentMessage />
+      <ContentMessage data={messages} />
       <Send />
     </div>
   );
-};
-
-export default Chat;
+}
--- src/components/Mailbox/Message/ContentMessage.tsx
+++ src/components/Mailbox/Message/ContentMessage.tsx
@@ -1,20 +1,39 @@
-import Image from "next/image";
+"use client";
 
-const ContentMessage = () => {
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+interface ContentMessageProps {
+  data: {
+    senderId: string;
+    receiverId: string;
+    content: string;
+  }[];
+}
+
+export default function ContentMessage({ data }: ContentMessageProps) {
+  const [totalComments, setTotalComments] = useState(data || []);
+
+  useEffect(() => {
+    setTotalComments(data || []);
+  }, [data]);
+
   return (
-    <div className="flex h-full flex-row items-end ">
-      <Image
-        src={"/public/avatar.jpg"}
-        alt="Profile picture"
-        width={50}
-        height={50}
-        className="mb-6 ml-3 rounded-full border border-solid border-black"
-      />
-      <p className="m-4 rounded-lg bg-indianred p-4 text-end text-white">
-        Messagehehehhehehhehhe
-      </p>
+    <div className="flex h-full flex-row items-end">
+      {totalComments && totalComments.map((message, index) => (
+        <div key={index}>
+          <Image
+            src={"/public/avatar.jpg"}
+            alt="Profile picture"
+            width={50}
+            height={50}
+            className="mb-6 ml-3 rounded-full border border-solid border-black"
+          />
+          <p className="m-4 rounded-lg bg-indianred p-4 text-end text-white">
+            {message.content}
+          </p>
+        </div>
+      ))}
     </div>
   );
-};
-
-export default ContentMessage;
+}
